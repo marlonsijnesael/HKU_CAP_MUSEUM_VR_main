e235cd93021f8e8de70621c77892e13227ea865a
J.v.Garderen <j.v.garderen93@gmail.com>
2018-11-25 11:19:50 +0100

    ported some menu things from my test project

---
+++ Museum VR/Assets/VolumeSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour {
+	public Slider volumeSlider;
+	// Use this for initialization
+	void Start () {
+		if(Input.GetKeyUp(KeyCode.Space)) {
+			Debug.Log("");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnSliderChanged(float value)
+	{
+		//Debug.Log(value);
+		volumeSlider.value = Mathf.Round(value * 10) / 10;
+	}
+}
---
+++ Museum VR/Assets/src/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public float menuTimeVisable;
+    private IEnumerator coroutine;
+	public GameObject mainMenu;
+	public GameObject settingMenu;
+
+	public AudioMixer masterMixer;
+    // Use this for initialization
+    void Start () {
+        //coroutine = WaitAndPrint(menuTimeVisable);
+        //StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+	public void SetMasterVolume(float mastervolume)
+	{
+		masterMixer.SetFloat("MaserVolume",mastervolume);
+	}
+
+	public void StartApp()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void OpenSettings()
+	{
+		mainMenu.SetActive(false);
+		settingMenu.SetActive(true);
+	}
+
+	public void CloseSettings()
+	{
+		mainMenu.SetActive(true);
+		settingMenu.SetActive(false);
+	}
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Debug.Log("Done");
+            //startMenuCanvas.SetActive(false);
+            StopCoroutine(coroutine);
+        }
+    }
+}
