989b396c3b609509646869a547d9bdfde26496cf
edv576 <31450210+edv576@users.noreply.github.com>
2018-12-10 14:12:19 +0100

    Change in target behaviour

--- museum main/Assets/Scripts/TargetBehaviour.cs
+++ museum main/Assets/Scripts/TargetBehaviour.cs
@@ -42,7 +42,7 @@ public class TargetBehaviour : MonoBehaviour {
 
     }
 
-    IEnumerator ChangeParentsSlow()
+    IEnumerator ChangeParentSlow()
     {
 
         if ((tempCollider == candleCollider) && !hasCandle && IsInRange(candle.transform.rotation.eulerAngles.x))
@@ -64,7 +64,7 @@ public class TargetBehaviour : MonoBehaviour {
 
         print("collides");
         tempCollider = other;
-        StartCoroutine("ChangeParentsSlow");
+        StartCoroutine("ChangeParentSlow");
         //if (other == candleCollider && !hasCandle)
         //{
         //    candle.transform.parent = this.transform;
@@ -74,4 +74,36 @@ public class TargetBehaviour : MonoBehaviour {
 
 
     }
+
+    //public GameObject raySpawnPoint;  //add empty gameObject to right hand, place it a bit in front of the actual hand so it won't hit the actual hand
+
+    //public bool holdingItem = false; //check if player is able to pick up item
+
+
+    //public GameObject itemSlot; //empty gameobject, you parent the item to this object, you have to allign the orientation of this object with the handmodel
+    //public GameObject item; //the actual item
+
+    //private void CastRayFromHand()
+    //{
+    //    RaycastHit hit;
+    //    if (!Physics.Raycast(raySpawnPoint.transform.position, raySpawnPoint.transform.forward, out hit))
+    //        return;
+
+    //    if (hit.transform.tag == "Candle" && !holdingItem)
+    //    {
+    //        //pass in the hit object to the grabItem function
+    //        GrabItem(hit);
+    //    }
+    //}
+
+    //public void GrabItem(RaycastHit hit)
+    //{
+    //    holdingItem = true;
+    //    item = hit.transform.gameObject;
+    //    item.transform.SetParent(itemSlot.transform);
+
+    //    item.transform.rotation = itemSlot.transform.rotation;
+    //    item.transform.position = itemSlot.transform.position;
+    //    //Destroy(item.GetComponent<Rigidbody>());
+    //}
 }
