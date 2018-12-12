edb1ea35c565c7e44afce0aa56f3befefa8923e8
edv576 <31450210+edv576@users.noreply.github.com>
2018-12-12 13:17:30 +0100

    hands motion

--- museum main/Assets/FMOD IMPL/F_Template.cs
+++ museum main/Assets/FMOD IMPL/F_Template.cs
@@ -4,6 +4,8 @@ public class F_Template : MonoBehaviour
 {
     [FMODUnity.EventRef]
     public string f_event;
+    [FMODUnity.EventRef]
+    public string[] _events;
 
     [SerializeField]
     public FMOD.Studio.EventInstance f_Instance;
@@ -17,8 +19,17 @@ public class F_Template : MonoBehaviour
   
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ChangeSound();
+        }
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(f_Instance, GetComponent<Transform>(), GetComponent<Rigidbody>());
     }
 
+    private void ChangeSound()
+    {
+        f_Instance = FMODUnity.RuntimeManager.CreateInstance(_events[Random.Range(0,_events.Length)]);
+        f_Instance.start();
+    }
    
 }
